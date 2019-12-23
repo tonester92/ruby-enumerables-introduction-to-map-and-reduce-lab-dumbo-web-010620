6d2ac9e35315f5ae7d07e24b8486badaaae2cc38
Antonio Blandin <antonio.blandin25@gmail.com>
2019-12-23 03:37:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,11 @@
 # My Code here....
 
-
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(source_array[i] * -1)
+    i += 1
+  end
+  return new
+end
